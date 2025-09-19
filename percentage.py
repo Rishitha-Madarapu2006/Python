0923b52e727265efd0be459c2ffd23de5808a862
Rishitha-Madarapu2006 <madarapurishitharao@gmail.com>
2025-09-19 21:10:24 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,12 @@
+marks=int(input())
+if(marks>=90):
+    print("Grade A")
+elif(marks>=70):
+    print("Grade B")
+elif(marks>=50):
+    print("Grade C")
+elif(marks>=35):
+    print("Grade D")
+else:
+    print("Fail")
+    
